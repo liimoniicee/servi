ff2df6128a34c0f5c3bbae9fd458c88109c207c3
liimoniicee <liimoniicee@gmail.com>
2021-05-04 02:08:38 -0600

    Create registro.php

---
+++ registro.php
@@ -0,0 +1,108 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+  <title>Login</title>
+
+  <!-- Canonical SEO -->
+
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width" />
+
+
+     <!-- Bootstrap core CSS     -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+\
+
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet" />
+
+</head>
+
+
+
+
+<body>
+
+  <header id="page-header" class="clearfix">
+  		<div class="container-fluid" >
+  	<div class="row-fluid">
+
+  				<div class="col-md-8 col-md-offset-2">
+  			<div class="header-text">
+  				<a class="logo" href="#" title="Página Principal (home)">
+  					<img src="imag/img-log-complet.png" class="img-responsive" alt="logo">				</a>
+  			</div>
+  		</div>
+
+  				</div>
+  			</div>
+  </header>
+  <div class="content" style="padding: 50px;">
+
+  <div class="row">
+                      	<div class="container col-md-8">
+                          	<div class="header-text text-center">
+                              	<h2>Servicio social ITES Los Cabos</h2>
+                              	<h4>REGISTRO</h4>
+
+                          	</div>
+                      	</div>
+                          <div class="container col-md-8" style="background-color:aliceblue; padding: 50px;">
+
+                      	<div class="col-md-4 mx-auto">
+                          <h3 class="text-center">Datos personales</h3>
+                          <hr>
+                          <form>
+
+    <div class="form-group">
+    <label for="Nombre">Nombre</label>
+    <input type="text" class="form-control" id="nombre" placeholder="Nombre">
+    </div>
+    <div class="form-group">
+      <label for="apep">Apellido paterno</label>
+      <input type="text" class="form-control" id="apep" placeholder="Apellido paterno">
+    </div>
+    <div class="form-group">
+      <label for="apem">Apellido materno</label>
+      <input type="text" class="form-control" id="apem" placeholder="Apellido materno">
+    </div>
+    <div class="form-group">
+      <label for="exampleInputEmail1">Email address</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    </div>
+    <div class="form-group">
+      <label for="password">Contrasena</label>
+      <input type="password" class="form-control" id="pass" placeholder="Contrasena">
+    </div>
+  </br>
+    <h3 class="text-center">Escolaridad</h3>
+    <hr>
+    <div class="form-group">
+      <label for="car">Carrera</label>
+      <input type="text" class="form-control" id="car" placeholder="Carrera">
+    </div>
+    <div class="form-group">
+      <label for="sem">Semestre</label>
+      <input type="text" class="form-control" id="sem" placeholder="Semestre">
+    </div>
+    <div class="form-group">
+      <label for="nc">Numero de control</label>
+      <input type="text" class="form-control" id="nc" placeholder="Numero de control">
+    </div>
+    <div class="form-group">
+      <label for="peri">Periodo</label>
+      <input type="text" class="form-control" id="peri" placeholder="Periodo">
+    </div>
+
+</br>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+                  	</div>
+                    	</div>
+              	</div>
+                  	</div>
+</body>
+
+  <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
